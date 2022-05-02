9dbceebe6a2982e1a9f3ffadad8d49e423cf8ab0
yuszafar <47925362+yuszafar@users.noreply.github.com>
2022-05-02 10:22:47 +0500

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -1,5 +1,5 @@
 BASEDIR=$(dirname $0)
 export $(cat $BASEDIR/dump.env)
 #dump DB
-mkdir -p $BASEDIR/dump
+mkdir -p $BASEDIR/dumps
 sudo -su postgres pg_dump $DUMP_DB_NAME > $BASEDIR/dumps/"dump_${DUMP_DB_NAME}_db_$(date +%Y-%m-%d__%H-%M).sql"
